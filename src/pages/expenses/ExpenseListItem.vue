3558be8319d2349752200e488bbf5e7c8471ae9f
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-04-10 13:19:27 -0300

    Added ExpenseListItem component

---
+++ src/pages/expenses/ExpenseListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="expense-list-item row">
+
+    <div class="col-10">
+      <div>{{data.description}}</div>
+      <small v-data-format="data.createdAt"/>
+    </div>
+    <div class="col-2" v-money-format="data.value"/>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ExpenseListItem',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.expense-list-item {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid var(--dark-medium)
+}
+</style>
